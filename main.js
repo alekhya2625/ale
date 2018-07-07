b9e83cd74ffe688b885510190fa4137ee3b4e5fb
alpha2625 <tatikonda.alekhya@gmail.com>
2018-07-07 13:56:29 +0530

    details

--- main.js
+++ main.js
@@ -13,5 +13,34 @@ function getfile(file,callback){
 getfile("data.json",function(text){
   let data = JSON.parse(text);
   console.log(data);
+  career(data.career);
+  education(data.education);
+
+})
+var child2=document.querySelector(".childTwo");
+function career(careerInfo){
+console.log(careerInfo);
+var careerHeading=document.createElement("h3");
+child2.appendChild(careerHeading);
+careerHeading.textContent="Career Objective";
+var careerHr=document.createElement("hr");
+child2.appendChild(careerHr);
+
+
+var ch=document.createElement("p");
+ch.textContent=careerInfo.info;
+child2.appendChild(ch);
 }
-)
+function education(edu){
+var eduHeading=document.createElement("h3");
+eduHeading.textContent="EDUCATIONAL QUALIFICATION";
+child2.appendChild(eduHeading);
+var careerHr=document.createElement("hr");
+child2.appendChild(careerHr);
+
+var eduTable=document.createElement("table");
+eduTable.border="1";
+var tr1="<tr><td>Degree</td><td>INSTITUTE</td><td>Data</td></tr>";
+eduTable.innerHTML=tr1;
+child2.appendChild(eduTable);
+}
